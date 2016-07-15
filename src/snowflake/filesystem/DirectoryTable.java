fa19f48862e1a0a297bd8fdee39a12cd025ba5a8
Johannes B. Latzel <johannes.latzel@web.de>
2016-07-15 18:55:28 +0200

    uses "j3l.util.Checker" instead of "j3l.util.check.ElementChecker"

--- src/snowflake/filesystem/DirectoryTable.java
+++ src/snowflake/filesystem/DirectoryTable.java
@@ -15,7 +15,7 @@ import snowflake.api.StorageException;
  * <p></p>
  * 
  * @since JDK 1.8
- * @version 2016.07.11_0
+ * @version 2016.07.15_0
  * @author Johannes B. Latzel
  */
 public final class DirectoryTable extends FileSystemDataTable<Directory, DirectoryData> {
