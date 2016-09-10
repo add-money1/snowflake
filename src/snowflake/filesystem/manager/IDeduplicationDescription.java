5e668cd7c5667bce699053bbaf42f2c1b4ce12e1
Johannes B. Latzel <johannes.latzel@web.de>
2016-09-10 16:41:32 +0200

    removed "int getDeducplicationLevel()"

--- src/snowflake/filesystem/manager/IDeduplicationDescription.java
+++ src/snowflake/filesystem/manager/IDeduplicationDescription.java
@@ -6,19 +6,10 @@ import snowflake.api.IAttributeValue;
  * <p></p>
  * 
  * @since JDK 1.8
- * @version 2016.07.23_0
+ * @version 2016.09.10_0
  * @author Johannes B. Latzel
  */
 public interface IDeduplicationDescription extends IAttributeValue<IDeduplicationDescription> {
-	
-	
-	/**
-	 * <p></p>
-	 *
-	 * @param
-	 * @return
-	 */
-	int getDeduplicationLevel();
 	
 	
 	/**
