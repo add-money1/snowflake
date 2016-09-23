445bdd83cc4daaef8e5360f0983654db32c60391
Johannes B. Latzel <johannes.latzel@web.de>
2016-09-23 17:44:35 +0200

    added some temporary impl.

--- src/snowflake/SnowFlake.java
+++ src/snowflake/SnowFlake.java
@@ -1,6 +1,7 @@
 package snowflake;
 
 import java.io.IOException;
+import java.nio.file.Path;
 
 import snowflake.api.IStorageInformation;
 import snowflake.core.storage.Storage;
@@ -11,10 +12,29 @@ import snowflake.filesystem.FileSystem;
  * <p></p>
  * 
  * @since JDK 1.8
- * @version 2016.06.11_0
+ * @version 2016.09.23_0
  * @author Johannes B. Latzel
  */
 public final class SnowFlake implements AutoCloseable {
+	
+	
+	/**
+	 * <p></p>
+	 *
+	 * @param
+	 * @return
+	 */
+	public static Storage format(Path path) {
+		// erstelle Storage in Datei oder Datenträger path
+		
+		// tabelle in blockform über speicher verteilt, sodass storage in einer datei, bzw
+		// auf einem Datenträger abgelegt werden kann (mergen von storage.data und chunk.table).6.
+		// erste n * 33 + 8 Bytes für n chunks
+		// letzter eintrag ist pointer für nächste n chunks
+		System.out.println("This method has not been implemented!");
+		System.out.println(path);
+		return null;
+	}
 
 	
 	/**
